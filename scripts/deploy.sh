b22aaa15394540021cc4562d557dadc1ef270c73
surajmandalcell <surajmandalcell@gmail.com>
2017-03-13 11:50:59 +0530

    try #2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B master public upstream/master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -26,4 +26,4 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
